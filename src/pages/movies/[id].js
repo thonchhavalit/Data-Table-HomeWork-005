a281d779570a2775094ebfea6099fc58be7b0807
thonchhavalit <valitgamming@gmail.com>
2023-05-03 08:40:17 +0700

    modified

--- src/pages/movies/[id].js
+++ src/pages/movies/[id].js
@@ -7,23 +7,23 @@ export default function About({ trailers }) {
   const router = useRouter();
   const [m, setMovie] = useState(JSON.parse(router.query.movie));
   const [key, setKey] = useState("")
-  useEffect(() =>  {
-    setKey(trailers[trailers.length -1].key)
+  useEffect(() => {
+    setKey(trailers[trailers.length - 1].key)
   }, [])
-  
+
 
   return (
     <Layout>
-        <div className="container pt-4 pb-5">
+      <div className="container pt-4 pb-5">
         <div className="d-flex flex-row justify-content-between">
-        <div className="me-4">
-          <img className="rounded-3" src={BASE_PATH + m.backdrop_path} alt="movie" width={400}></img>
-        </div>
-        <div>
-          <h1>{m.original_title}</h1>
-          <p>Overview</p>
-          <p>{m.overview}</p>
-          <button
+          <div className="me-4">
+            <img className="rounded-3" src={BASE_PATH + m.backdrop_path} alt="movie" width={400}></img>
+          </div>
+          <div>
+            <h1>{m.original_title}</h1>
+            <p>Overview</p>
+            <p>{m.overview}</p>
+            <button
               type="button"
               class="btn btn-primary"
               data-bs-toggle="modal"
@@ -40,17 +40,17 @@ export default function About({ trailers }) {
             >
               <div class="modal-dialog  modal-dialog-centered modal-xl ">
                 <div class="modal-content">
-                  
-                  <iframe style={{height: 687}} src={YT + key} frameborder="0" allowfullscreen></iframe>
-                
+
+                  <iframe style={{ height: 687 }} src={YT + key} frameborder="0" allowfullscreen></iframe>
+
                 </div>
               </div>
             </div>
 
+          </div>
         </div>
       </div>
-        </div>
-    
+
     </Layout>
   );
 }
@@ -60,10 +60,10 @@ export async function getServerSideProps({ params }) {
     ${BASE_URL}/movie/${params.id}/videos?api_key=${API_KEY}
     `
   );
-  const resp = await res.json();
+  const result = await res.json();
   return {
     props: {
-      trailers: resp.results,
+      trailers: result.results,
     },
   };
 }
