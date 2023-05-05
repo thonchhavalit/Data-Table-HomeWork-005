8cc03ae6345118e274fc40dff9190ce6e69c384b
thonchhavalit <valitgamming@gmail.com>
2023-05-05 08:28:08 +0700

    update

--- src/pages/datatable.js
+++ src/pages/datatable.js
@@ -26,7 +26,7 @@ function Datatable() {
             sortable: true,
         },
         {
-            name:"Category",
+            name: "Category",
             selector: "category",
             sortable: true,
         },
@@ -35,23 +35,32 @@ function Datatable() {
             selector: "images",
             sortable: true,
         },
+        {
+            name: "Action",
+            selector: "action",
+        },
     ];
+
 
     const rows = data.map((item) => ({
         title: item.title,
         price: item.price,
-        category:item.category.name,
-        images: <img src={item.images} style={{ height: '10rem', width: '10rem' }}></img> ,
-        
+        category: item.category.name,
+        images: <img src={item.images} style={{ height: '10rem', width: '10rem' }}></img>,
+        action: <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+            <button class="btn btn-outline-warning me-md-2" type="button">Edit</button>
+            <button class="btn btn-outline-danger" type="button">Delete</button>
+        </div>
+
     }));
     const [record, setRecord] = useState(data);
     function handlefilter(event) {
-        const newData = data.filter(rows =>{
+        const newData = record.filter(rows => {
             return rows.title.toLoweCase().includes(event.target.value.toLoweCase())
         })
         setRecord(newData)
     }
-        
+
 
     return (
         <Layout>
@@ -60,7 +69,7 @@ function Datatable() {
                     <div class="container-fluid justify-content-end">
                         <form class="d-flex" role="search">
                             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" onChange={handlefilter}></input>
-                                <button class="btn btn-outline-success" type="submit">Search</button>
+                            <button class="btn btn-outline-success" type="submit">Search</button>
                         </form>
                     </div>
                 </nav>
